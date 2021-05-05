4c7f83b8bc15bfd5e138f38705bd6d040e42d7a4
Sholto <madein.mania@gmail.com>
2021-05-05 14:58:01 +0100

    Skills container, mapping out the rest of the app in this one file as a static mock up

--- src/Components/Skills-Container.jsx
+++ src/Components/Skills-Container.jsx
@@ -1,3 +1,95 @@
 import React from 'react';
-import EditBox from './Edit-Box';
-import DisplayBox from './Display-Box';
+import PlaceHolder from './Name-Holder';
+
+class SkillsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { skills: [] };
+  }
+
+  render() {
+    return <button>Add</button>;
+  }
+}
+
+class WorkExperienceContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { skills: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Work Experience</h2>
+        </div>
+        <button>Add</button>
+      </div>
+    );
+  }
+}
+
+class JobContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { skills: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        <PlaceHolder name='Enter Job Name' />
+        <PlaceHolder name='Enter Job Title' />
+        <PlaceHolder name='From' />
+        <PlaceHolder name='To' />
+        <PlaceHolder name='Enter Some Details on Your Responsibilites/Achievements Here' />
+        <PlaceHolder name='Enter Some Details on Your Responsibilites/Achievements Here' />
+        <PlaceHolder name='Enter Some Details on Your Responsibilites/Achievements Here' />
+        <button>Add</button>
+      </div>
+    );
+  }
+}
+
+class EducationContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { skills: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Education History</h2>
+        </div>
+        <button>Add</button>
+      </div>
+    );
+  }
+}
+
+class EduContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { skills: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        <PlaceHolder name='Enter Qualification Name' />
+        <PlaceHolder name='Enter Education Body' />
+        <PlaceHolder name='From' />
+        <PlaceHolder name='To' />
+        <PlaceHolder name='Enter Some Details on Your Achievements Here' />
+        <PlaceHolder name='Enter Some Details on Your Achievements Here' />
+        <PlaceHolder name='Enter Some Details on Your Achievements Here' />
+        <button>Add</button>
+      </div>
+    );
+  }
+}
+
+export default SkillsContainer;
