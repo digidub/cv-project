7a34dc1e433f85d30b36a467272627130fa01ffe
Sholto <madein.mania@gmail.com>
2021-05-05 11:40:46 +0100

    additional placeholders

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import PlaceHolder from './Components/Name-Holder';
 ReactDOM.render(
   <React.StrictMode>
     <PlaceHolder name='Enter Name Here' />
+    <PlaceHolder name='Telephone Number' />
+    <PlaceHolder name='Email' />
+    <PlaceHolder name='Website' />
   </React.StrictMode>,
   document.getElementById('root')
 );
