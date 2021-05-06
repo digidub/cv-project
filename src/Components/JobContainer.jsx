93ddda882f0ef7e0bfc60aabc47f47fe75cee8e9
Sholto <madein.mania@gmail.com>
2021-05-06 12:43:25 +0100

    Boilerplate for individual job component

---
+++ src/Components/JobContainer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class JobContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { skills: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        <Placeholder name='Enter Job Name' />
+        <Placeholder name='Enter Job Title' />
+        <Placeholder name='From' />
+        <Placeholder name='To' />
+        <Placeholder name='Enter Some Details on Your Responsibilites/Achievements Here' />
+        <Placeholder name='Enter Some Details on Your Responsibilites/Achievements Here' />
+        <Placeholder name='Enter Some Details on Your Responsibilites/Achievements Here' />
+        <button>Add</button>
+      </div>
+    );
+  }
+}
+
+export default JobContainer;
