94792712a3b1aaaf023b1fb64d0e5f97ab402fb4
Sholto <madein.mania@gmail.com>
2021-05-06 12:55:01 +0100

    updated logic for adding Job Containers

--- src/Components/WorkExperienceContainer.jsx
+++ src/Components/WorkExperienceContainer.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import maxKeyInArray from './AppLogic';
+import JobContainer from './JobContainer';
 
 class WorkExperienceContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { skills: [] };
+    this.state = { jobs: [{ id: 1 }] };
   }
 
+  handleClick = () => {
+    const x = maxKeyInArray(this.state.jobs);
+    this.setState((state) => ({
+      jobs: state.jobs.concat({ id: x + 1 }),
+    }));
+  };
+
   render() {
+    const jobs = this.state.jobs.map((job) => {
+      return <JobContainer key={job.id} />;
+    });
     return (
       <div>
         <div>
           <h2>Work Experience</h2>
         </div>
-        <button>Add</button>
+        <div>{jobs}</div>
+        <button onClick={this.handleClick}>Add</button>
       </div>
     );
   }
