6b24668bb9008e28b247641f574e17f0e4762242
Sholto <madein.mania@gmail.com>
2021-05-06 12:39:08 +0100

    import Skills Container

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import TitleContainer from './Components/TitleContainer';
 import DocumentContainer from './Components/DocumentContainer';
+import SkillsContainer from './Components/SkillsContainer';
 
 ReactDOM.render(
   <React.StrictMode>
     <DocumentContainer>
       <TitleContainer />
+      <SkillsContainer />
     </DocumentContainer>
   </React.StrictMode>,
   document.getElementById('root')
