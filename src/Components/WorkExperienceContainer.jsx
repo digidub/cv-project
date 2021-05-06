7259176d6a0141889ec5c7a1583f89355bb556cc
Sholto <madein.mania@gmail.com>
2021-05-06 12:42:19 +0100

    renamed to a more suitable name

---
+++ src/Components/WorkExperienceContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class WorkExperienceContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { skills: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Work Experience</h2>
+        </div>
+        <button>Add</button>
+      </div>
+    );
+  }
+}
+
+export default WorkExperienceContainer;
