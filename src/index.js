d60586eb0816d0e8f68d00efcb857fec8a3426d1
Sholto <madein.mania@gmail.com>
2021-05-06 12:55:15 +0100

    added WorkExperienceContainer to dom flow

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import TitleContainer from './Components/TitleContainer';
 import DocumentContainer from './Components/DocumentContainer';
 import SkillsContainer from './Components/SkillsContainer';
+import WorkExperienceContainer from './Components/WorkExperienceContainer';
 
 ReactDOM.render(
   <React.StrictMode>
     <DocumentContainer>
       <TitleContainer />
       <SkillsContainer />
+      <WorkExperienceContainer />
     </DocumentContainer>
   </React.StrictMode>,
   document.getElementById('root')
