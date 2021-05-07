16fe35293cbde782904d8c17baff0e27b48baa55
Sholto <madein.mania@gmail.com>
2021-05-07 08:41:07 +0100

    added logic for generating additional qualification rows

--- src/Components/EduContainer.jsx
+++ src/Components/EduContainer.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
+import maxKeyInArray from './AppLogic';
 import Placeholder from './ValuePlaceholder';
 
 class EduContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { skills: [] };
+    this.state = { quals: [{ id: 1, name: 'Enter details of qualifications here' }] };
   }
 
+  handleClick = () => {
+    const x = maxKeyInArray(this.state.quals);
+    this.setState((state) => ({
+      quals: state.quals.concat({ id: x + 1, name: 'Enter details of qualifications here' }),
+    }));
+  };
+
   render() {
+    const quals = this.state.quals.map((qual) => {
+      return <Placeholder key={qual.id} name={qual.name} />;
+    });
     return (
       <div>
         <Placeholder name='Enter Qualification Name' />
         <Placeholder name='Enter Education Body' />
         <Placeholder name='From' />
         <Placeholder name='To' />
-        <Placeholder name='Enter Some Details on Your Achievements Here' />
-        <Placeholder name='Enter Some Details on Your Achievements Here' />
-        <Placeholder name='Enter Some Details on Your Achievements Here' />
-        <button>Add</button>
+        {quals}
+        <button onClick={this.handleClick}>Add Row</button>
       </div>
     );
   }
