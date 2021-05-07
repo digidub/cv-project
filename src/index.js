e1b195c48e35f32f82cd720d013742bc6172d744
Sholto <madein.mania@gmail.com>
2021-05-07 16:19:41 +0100

    layout of components

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import TitleContainer from './Components/TitleContainer';
 import DocumentContainer from './Components/DocumentContainer';
 import SkillsContainer from './Components/SkillsContainer';
 import WorkExperienceContainer from './Components/WorkExperienceContainer';
+import EduExperienceContainer from './Components/EduExperienceContainer';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <DocumentContainer>
-      <TitleContainer />
-      <SkillsContainer />
-      <WorkExperienceContainer />
-    </DocumentContainer>
+    <App>
+      <DocumentContainer>
+        <TitleContainer />
+        <SkillsContainer />
+        <WorkExperienceContainer />
+        <EduExperienceContainer />
+      </DocumentContainer>
+    </App>
   </React.StrictMode>,
   document.getElementById('root')
 );
