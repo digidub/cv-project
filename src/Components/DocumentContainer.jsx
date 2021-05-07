5dc989a760d72994c96d3c3e3370923cc07a63ec
Sholto <madein.mania@gmail.com>
2021-05-07 16:20:30 +0100

    removed needless constructor, stateless component

--- src/Components/DocumentContainer.jsx
+++ src/Components/DocumentContainer.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import './DocumentContainer.css';
 
 class DocumentContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { canEdit: true };
-  }
   render() {
     return (
       <div className='document-container'>
         {React.Children.map(this.props.children, (child) => {
           if (React.isValidElement(child)) {
-            return React.cloneElement(child, { canEdit: this.state.canEdit });
+            return React.cloneElement(child, { canEdit: this.props.canEdit });
           }
         })}
       </div>
