134e185ab8fa58d3b87ac9b072ed9293d497ac51
Sholto <madein.mania@gmail.com>
2021-05-07 16:17:21 +0100

    main app component to hold document viewer and surrounding logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React from 'react';
 import './App.css';
+import Toggle from 'react-toggle';
+import 'react-toggle/style.css';
 
-function App() {
-  return <div className='App'></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { canEdit: true };
+  }
+
+  handleChange = () => {
+    this.setState((prevState) => ({
+      canEdit: !prevState.canEdit,
+    }));
+  };
+
+  render() {
+    return (
+      <div className='app'>
+        <div className='header'>
+          <Toggle defaultChecked={this.state.canEdit} onChange={this.handleChange} />
+          <span>Edit Mode?</span>
+        </div>
+        <div className='body'>
+          {React.Children.map(this.props.children, (child) => {
+            if (React.isValidElement(child)) {
+              return React.cloneElement(child, { canEdit: this.state.canEdit });
+            }
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
