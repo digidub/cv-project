245e0e0d672d7c421628bf1ae3ed9353dbbcb363
Sholto <madein.mania@gmail.com>
2021-05-07 09:26:58 +0100

    logic added that makes added rows of job responsibilities auto edit mode

--- src/Components/JobContainer.jsx
+++ src/Components/JobContainer.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
+import maxKeyInArray from './AppLogic';
 import Placeholder from './ValuePlaceholder';
 
 class JobContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { skills: [] };
+    this.state = {
+      roles: [
+        { id: 1, name: 'Enter Some Details on Your Responsibilites/Achievements Here' },
+        { id: 2, name: 'Enter Some Details on Your Responsibilites/Achievements Here' },
+        { id: 3, name: 'Enter Some Details on Your Responsibilites/Achievements Here' },
+      ],
+    };
   }
 
+  handleClick = () => {
+    const x = maxKeyInArray(this.state.roles);
+    this.setState((state) => ({
+      roles: state.roles.concat({ id: x + 1, name: 'Enter Some Details on Your Responsibilites/Achievements Here', edit: 'true' }),
+    }));
+  };
+
   render() {
+    const roles = this.state.roles.map((role) => {
+      return <Placeholder key={role.id} name={role.name} edit={role.edit ?? false} />;
+    });
     return (
       <div>
         <Placeholder name='Enter Job Name' />
         <Placeholder name='Enter Job Title' />
         <Placeholder name='From' />
         <Placeholder name='To' />
-        <Placeholder name='Enter Some Details on Your Responsibilites/Achievements Here' />
-        <Placeholder name='Enter Some Details on Your Responsibilites/Achievements Here' />
-        <Placeholder name='Enter Some Details on Your Responsibilites/Achievements Here' />
-        <button>Add</button>
+        {roles}
+        <button onClick={this.handleClick}>Add</button>
       </div>
     );
   }
