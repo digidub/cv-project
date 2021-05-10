6c6ce44e943856f3e43e6a5e2fb763b822efe2f5
Sholto <madein.mania@gmail.com>
2021-05-10 09:55:50 +0100

    delete value placholders functionality

--- src/Components/SkillsContainer.jsx
+++ src/Components/SkillsContainer.jsx
@@ -16,18 +16,26 @@ class SkillsContainer extends React.Component {
     };
   }
 
-  handleClick = () => {
+  handleAdd = () => {
     const x = maxKeyInArray(this.state.placeholder);
     this.setState((state) => ({
       placeholder: state.placeholder.concat({ id: x + 1, name: 'enter name here' }),
     }));
   };
 
+  handleDelete = (e) => {
+    console.log(e);
+    const toDelete = e.target.previousSibling.previousSibling.textContent;
+    this.setState((state) => ({
+      placeholder: state.placeholder.filter((word) => word.name !== toDelete),
+    }));
+  };
+
   render() {
     let canEdit;
-    if (this.props.canEdit) canEdit = <button onClick={this.handleClick}>Add</button>;
+    if (this.props.canEdit) canEdit = <button onClick={this.handleAdd}>Add</button>;
     const placeholderItems = this.state.placeholder.map((name, index) => {
-      return <Placeholder name={name.name} key={name.id} canEdit={this.props.canEdit} />;
+      return <Placeholder name={name.name} key={name.id} canEdit={this.props.canEdit} canDelete={this.handleDelete} />;
     });
     return (
       <div className='skills-container'>
