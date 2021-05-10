2bd813d7a34390c7049a6f8038a48a93391d1fe4
Sholto <madein.mania@gmail.com>
2021-05-10 10:36:20 +0100

    logic for deleting job containers and also handling job ID when array is 0

--- src/Components/WorkExperienceContainer.jsx
+++ src/Components/WorkExperienceContainer.jsx
@@ -6,21 +6,30 @@ import './WorkExperienceContainer.css';
 class WorkExperienceContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { jobs: [{ id: 1 }] };
+    this.state = { jobs: [{ id: 1, name: 'Enter Job Title' }] };
   }
 
-  handleClick = () => {
-    const x = maxKeyInArray(this.state.jobs);
+  handleAdd = () => {
+    let x;
+    if (this.state.jobs.length < 1) x = 1;
+    else x = maxKeyInArray(this.state.jobs);
     this.setState((state) => ({
-      jobs: state.jobs.concat({ id: x + 1 }),
+      jobs: state.jobs.concat({ id: x + 1, name: 'Enter Job Title' }),
+    }));
+  };
+
+  handleDelete = (e) => {
+    const toDelete = e.target.parentNode.parentNode.parentNode.dataset.key;
+    this.setState((state) => ({
+      jobs: state.jobs.filter((job) => job.id != toDelete),
     }));
   };
 
   render() {
     let canEdit;
-    if (this.props.canEdit) canEdit = <button onClick={this.handleClick}>Add</button>;
+    if (this.props.canEdit) canEdit = <button onClick={this.handleAdd}>Add</button>;
     const jobs = this.state.jobs.map((job) => {
-      return <JobContainer key={job.id} canEdit={this.props.canEdit} />;
+      return <JobContainer name={job.name} key={job.id} dataKey={job.id} canEdit={this.props.canEdit} canDelete={this.handleDelete} />;
     });
     return (
       <div className='work-experience-container'>
