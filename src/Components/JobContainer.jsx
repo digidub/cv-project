145ab46c98c88622fe3da75301dc2be3e5b03fc6
Sholto <madein.mania@gmail.com>
2021-05-10 10:38:48 +0100

    data-key for job id

--- src/Components/JobContainer.jsx
+++ src/Components/JobContainer.jsx
@@ -18,28 +18,31 @@ class JobContainer extends React.Component {
   handleClick = () => {
     const x = maxKeyInArray(this.state.roles);
     this.setState((state) => ({
-      roles: state.roles.concat({ id: x + 1, name: 'Enter Some Details on Your Responsibilites/Achievements Here', edit: 'true' }),
+      roles: state.roles.concat({ id: x + 1, name: 'Enter Some Details on Your Experience Here', edit: 'true' }),
     }));
   };
 
   render() {
     let canEdit;
+    let canDelete;
     if (this.props.canEdit) canEdit = <button onClick={this.handleClick}>Add Row</button>;
+    if (this.props.canEdit && this.props.canDelete) canDelete = <button onClick={this.props.canDelete}>Delete Job</button>;
     const roles = this.state.roles.map((role) => {
       return <Placeholder key={role.id} name={role.name} edit={role.edit ?? false} canEdit={this.props.canEdit} />;
     });
     return (
-      <div className='job-container'>
+      <div className='job-container' data-key={this.props.dataKey}>
         <div className='job-heading'>
           <div className='job-title'>
-            <Placeholder name='Enter Company Name' canEdit={this.props.canEdit} />
+            <Placeholder name={this.props.name} canEdit={this.props.canEdit} />
             <p> - </p>
-            <Placeholder name='Enter Job Title' canEdit={this.props.canEdit} />
+            <Placeholder name='Enter Company Name' canEdit={this.props.canEdit} />
           </div>
           <div className='job-from-to'>
             <Placeholder name='From' canEdit={this.props.canEdit} />
             <p> - </p>
             <Placeholder name='To' canEdit={this.props.canEdit} />
+            {canDelete}
           </div>
         </div>
         <div className='job-roles'>{roles}</div>
