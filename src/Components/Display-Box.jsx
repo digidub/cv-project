76924d22d62ec8510aa463d1fcdd61d1bf032bb7
Sholto <madein.mania@gmail.com>
2021-05-10 11:37:58 +0100

    conditional deletion logic

--- src/Components/Display-Box.jsx
+++ src/Components/Display-Box.jsx
@@ -20,7 +20,7 @@ class DisplayBox extends React.Component {
     if (this.props.canEdit) canEdit = <button onClick={this.props.edit}>edit</button>;
     if (this.props.canEdit && this.props.canDelete) canDelete = <button onClick={this.props.canDelete}>delete</button>;
     return (
-      <div className={this.props.class}>
+      <div className={this.props.class} data-key={this.props.dataKey}>
         {this.props.value}
         {canEdit}
         {canDelete}
